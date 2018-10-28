6bf1afe2328edd9911e8223d62300d1a7ce1a979
Andrei Savastsijonak <Andddrews@gmail.com>
2018-10-28 16:18:05 +0300

    style: spaces

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,12 +1,12 @@
 function sumOfOther (a) {
 let sum = 0;
 let sumOf = [];
-a.forEach( (x) => sum +=x);
+a.forEach( (x) => sum += x);
 
-for (let i=0; i< a.length; i++){
+    for (let i=0; i < a.length; i++){
 
-sumOf.push(sum-a[i]); 
+    sumOf.push(sum-a[i]); 
 
-}
+    }
  return sumOf;
 }
